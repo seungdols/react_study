9eca0b2aedf0d70b6727b1db28dcb0c5e35ee630
seungdols <seungdols0822@gmail.com>
2018-08-24 01:19:34 +0900

    2장 JSX: 인라인 스타일링

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -4,8 +4,18 @@ class App extends Component {
   render() {
     const text = "당신은 어떠신가요?";
     const condition = true;
+    const style = {
+      backgroundColor: 'gray',
+      border: ' 1px solid black',
+      height: Math.round(Math.random() * 300) + 50,
+      width: Math.round(Math.random() * 300) + 50,
+      WebkitTransition: ' all',
+      MozTransition: ' all',
+      msTransition: ' all'
+    };
+
     return (
-      <Fragment>
+      <div>
         <h1>리액트 안녕!</h1>
         <h2>{text}</h2>
         {
@@ -14,7 +24,8 @@ class App extends Component {
         {
           condition && '보여주세요'
         }
-      </Fragment>
+        <div style={style}></div>
+      </div>
     );
   }
 }
