b074cbb76141613c0a8fef370cf173e92ecacaec
seungdols <seungdols0822@gmail.com>
2018-08-24 00:56:06 +0900

    2장 JSX: Fragment, Variable 사용

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
 
 class App extends Component {
   render() {
+    const text = "당신은 어떠신가요?";
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Fragment>
+        <h1>리액트 안녕!</h1>
+        <h2>{text}</h2>
+      </Fragment>
     );
   }
 }
