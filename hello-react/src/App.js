48fa56fc297c3f24aefaff636f0dd8e06e7f1ca3
seungdols <seungdols0822@gmail.com>
2018-08-24 01:16:45 +0900

    2장 JSX: 조건부 렌더링

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -3,10 +3,17 @@ import React, { Component, Fragment } from 'react';
 class App extends Component {
   render() {
     const text = "당신은 어떠신가요?";
+    const condition = true;
     return (
       <Fragment>
         <h1>리액트 안녕!</h1>
         <h2>{text}</h2>
+        {
+          condition ? 'True' : 'False'
+        }
+        {
+          condition && '보여주세요'
+        }
       </Fragment>
     );
   }
