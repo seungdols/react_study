2766b93e970a51b6cf7fb6a05cbca5878f502581
seungdols <seungdols0822@gmail.com>
2018-08-26 19:39:03 +0900

    4.2 event: event function의 분리

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -6,22 +6,32 @@ export default class EventPractice extends Component {
         message: ""
     }
 
+    constructor(props){
+        super(props)
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            message: e.target.value
+        })
+    }
+
+    handleClick() {
+        alert(this.state.message)
+        this.setState({
+            message: ""
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>이벤트 연습</h1>
                 <input type="text" name="message" value={this.state.message}
-                placeholder="input text in here" onChange={(e) => {
-                    this.setState({
-                        message: e.target.value
-                    })
-                }}/>
-                <button onClick={() => {
-                    alert(this.state.message)
-                    this.setState({
-                        message: ""
-                    })
-                }}>확인</button>
+                placeholder="input text in here" onChange={this.handleChange}/>
+                <button onClick={this.handleClick}>확인</button>
             </div>
         )
     }
