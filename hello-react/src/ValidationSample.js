0b03b9337af2137f11599a7031e9362d0e0bd7ad
seungdols <seungdols0822@gmail.com>
2018-08-27 14:11:53 +0900

    5.2 ref: input ref 달기 예제

--- hello-react/src/ValidationSample.js
+++ hello-react/src/ValidationSample.js
@@ -20,15 +20,15 @@ export default class ValidationSample extends Component {
             clicked: true,
             validated: this.state.password === '0000'
         })
-
+        this.input.focus();
     }
 
     render() {
         return (
             <div>
-                <input type="password" value={this.state.password} 
-                onChange={this.handleChange} 
-                className={this.state.clicked ? (this.state.validated ? 'success' : 'failure') : ''} 
+                <input ref={(ref) => {this.input=ref}} type="password" value={this.state.password}
+                onChange={this.handleChange}
+                className={this.state.clicked ? (this.state.validated ? 'success' : 'failure') : ''}
                 />
                 <button onClick={this.handleButtonClick}>검증하기</button>
             </div>
