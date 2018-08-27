2f19069ec2a7a7d859528ddc0b5102b3a9b3e8da
seungdols <seungdols0822@gmail.com>
2018-08-28 00:50:45 +0900

    6.4 컴포넌트 반복: input을 통한 데이터 추가

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
 
 export default class IterationSample extends Component {
+
+    state = {
+        names: ['눈사람','고양이','강아지','멍멍이','댕댕이'],
+        name: ""
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            name: e.target.value
+        });
+    }
+
+    handleInsert = () => {
+        this.setState({
+            names: this.state.names.concat(this.state.name),
+            name: ""
+        })
+    }
+
     render() {
-        const names = ['눈사람','고양이','강아지','멍멍이','댕댕이']
-        const nameList = names.map((name, index) => <li key={index}>{name}</li>)
+        const nameList = this.state.names.map((name, index) => <li key={index}>{name}</li>)
         return (
+            <div>
+            <input type="text"
+                onChange={this.handleChange}
+                value={this.state.name}/>
+            <button onClick={this.handleInsert}>추가</button>
             <ul>
                 {nameList}
             </ul>
+            </div>
         )
     }
 }
