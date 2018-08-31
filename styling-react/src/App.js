4aedc1ad5a61beba123e2354bb3f379c8073e3e5
seungdols <seungdols0822@gmail.com>
2018-08-31 16:27:30 +0900

    9장 classNames 라이브러리 사용

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import classNames from 'classnames'
 import styles from './App.css';
 
 console.log(styles)
 class App extends Component {
   render() {
     return (
-      <div className={ [styles.box, styles.blue].join('') }>
+      <div className={ classNames(styles.box, styles.blue) }>
 
       </div>
     );
