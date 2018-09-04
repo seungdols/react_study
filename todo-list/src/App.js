956d33fe5b2d99e6d92b9918a0eff97e314708a9
seungdols <seungdols0822@gmail.com>
2018-09-04 20:51:10 +0900

    10장 To do list: Add TodoList component

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import PageTemplate from './components/PageTemplate'
 import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
 export default class App extends Component {
     render() {
         return (
             <PageTemplate>
                 <TodoInput />
+                <TodoList />
             </PageTemplate>
         )
     }
