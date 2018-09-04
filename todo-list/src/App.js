733dc5dffdd58d8c9e6dd293957e3548fc534b2f
seungdols <seungdols0822@gmail.com>
2018-09-04 20:54:37 +0900

    10장: Add input state in App component

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -3,10 +3,27 @@ import PageTemplate from './components/PageTemplate'
 import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
 export default class App extends Component {
+
+    state = {
+        input: ""
+    }
+
+    handleChange = (e) => {
+        const {value} = e.target;
+        this.setState({
+            input: value
+        })
+    }
+
+
     render() {
+        const {input} = this.state;
+        const {
+            handleChange
+        } = this;
         return (
             <PageTemplate>
-                <TodoInput />
+                <TodoInput onChange={handleChange} value={input} />
                 <TodoList />
             </PageTemplate>
         )
