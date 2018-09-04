18f809879668607f4f2e67b61fcd8fefac021be5
seungdols <seungdols0822@gmail.com>
2018-09-04 19:39:50 +0900

    10장 To do list: add PageTemplate component

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
+import PageTemplate from './components/PageTemplate'
 
 export default class App extends Component {
     render() {
         return (
-            <div>
-                일정관리
-            </div>
+            <PageTemplate></PageTemplate>
         )
     }
 }
