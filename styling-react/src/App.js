f13fa4ee527882ae441b8719f75dc38b057f6e6c
seungdols <seungdols0822@gmail.com>
2018-09-04 15:54:29 +0900

    9장. 컴포넌트 스타일링: classNames/bind 오류 해결

--- styling-react/src/App.js
+++ styling-react/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import classNames from 'classnames';
+import classNames from 'classnames/bind';
 import styles from './App.css';
-
 const cx = classNames.bind(styles);
+
 
 class App extends Component {
   render() {
-    const isBlue = false;
+    const isBlue = true;
+
     return (
-      <div className={cx('box',{blue: isBlue})}>
+      <div className={cx('box', {blue: isBlue})}>
 
       </div>
     );
