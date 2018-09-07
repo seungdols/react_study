3e06c7727689844394c3c63240c174d9aab55e5e
seungdols <seungdols0822@gmail.com>
2018-09-07 23:56:55 +0900

    13장 CounterContainer 생성

--- redux-counter/src/containers/App.js
+++ redux-counter/src/containers/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import Counter from '../components/Counter';
+import CounterContainer from './CounterContainer'
 export default class App extends Component {
     render() {
         return (
             <div>
-                <Counter />
+                <CounterContainer />
             </div>
         )
     }
--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './containers/App'
 import './index.css'
-import { createStore } from 'redux';
+import { createStore } from 'redux'
 import reducers from './reducers'
 import { Provider } from 'react-redux'
 
-const store = createStore(reducers);
-
+const store = createStore(reducers)
 
 ReactDOM.render(
-<Provider store={store}>
-<App />
-</Provider>,
-document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
