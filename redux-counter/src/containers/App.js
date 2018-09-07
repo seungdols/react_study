3b0cf5fe1eee7196200c0a8ef8e13fc75d5fc767
seungdols <seungdols0822@gmail.com>
2018-09-07 22:52:36 +0900

    13장 Counter 그려보기

--- redux-counter/src/containers/App.js
+++ redux-counter/src/containers/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
+import Counter from '../components/Counter';
 export default class App extends Component {
     render() {
         return (
             <div>
-                Counter
+                <Counter />
             </div>
         )
     }
