afd63ca89b217717e3463a128a517ceb4e1f1f5b
seungdols <seungdols0822@gmail.com>
2018-09-07 22:57:47 +0900

    13장 Provider 컴포넌트로 store 연동

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -4,8 +4,13 @@ import App from './containers/App';
 import './index.css'
 import { createStore } from 'redux';
 import reducers from './reducers'
+import { Provider } from 'react-redux'
 
 const store = createStore(reducers);
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>,
+document.getElementById('root'));
