e09c047c1c306689f68cde777fb877e1b662c08f
seungdols <seungdols0822@gmail.com>
2018-09-09 12:00:58 +0900

    13장 다중 카운터에 맞게 App component 변경

--- redux-counter/src/containers/App.js
+++ redux-counter/src/containers/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
-import CounterContainer from './CounterContainer'
-export default class App extends Component {
+import CounterListContainer from './CounterListContainer'
+import Buttons from '../components/Buttons'
+import getRandomColor from '../lib/getRandomColor'
+import { connect } from 'react-redux'
+import * as actions from '../actions'
+
+class App extends Component {
     render() {
+        const {onCreate, onRemove} = this.props
         return (
-            <div>
-                <CounterContainer />
+            <div className="App">
+                <Buttons
+                    onCreate={onCreate}
+                    onRemove={onRemove}
+                    ></Buttons>
+                <CounterListContainer />
             </div>
         )
     }
 }
+
+const mapToDispatch = (dispatch) => ({
+    onCreate: () => dispatch(actions.create(getRandomColor())),
+    onRemove: (index) => dispatch(actions.remove(index))
+})
+
+export default connect(null, mapToDispatch)(App)
