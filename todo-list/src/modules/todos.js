1cf7bf7340fb2a3a4a00057962c8e5a11b789cb9
seungdols <seungdols0822@gmail.com>
2018-09-11 01:31:20 +0900

    14.4 Ducks 구조의 todos 모듈

---
+++ todo-list/src/modules/todos.js
@@ -0,0 +1,45 @@
+import { Map, List } from 'immutable'
+import { handleActions, createAction } from 'redux-actions'
+
+
+const INSERT = 'todos/INSERT'
+const TOGGLE = 'todos/TOGGLE'
+const REMOVE = 'todos/REMOVE'
+
+export const insert = createAction(INSERT)
+export const toggle = createAction(TOGGLE)
+export const remove = createAction(REMOVE)
+
+const initialState = List([
+  Map({
+    id: 0,
+    text: 'Learn to React',
+    done: true
+  }),
+  Map({
+    id: 1,
+    text: 'Try to component styling',
+    done: false
+  })
+])
+
+const default handleActions({
+  [INSERT]: (state, action) => {
+    const { id, text, done } = action.payload
+
+    return state.push(Map({
+      id,
+      text,
+      done
+    }))
+  },
+  [TOGGLE]: (state, action) => {
+    const { payload: index } = action
+
+    return state.updateIn([index, 'done'], done => !done)
+  },
+  [REMOVE]: (state, action) => {
+    const { payload: index } = action
+    return state.delete(index)
+  }
+}, initialState)
