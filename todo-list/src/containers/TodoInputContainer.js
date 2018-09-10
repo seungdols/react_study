911fdf81f7fc1e1a41b26228a3c9135acc0abdab
seungdols <seungdols0822@gmail.com>
2018-09-11 01:42:58 +0900

    14.4 TodoInputContainer component

---
+++ todo-list/src/containers/TodoInputContainer.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import TodoInput from '../components/TodoInput'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import * as inputActions from '../modules/index'
+import * as todosActions from '../modules/todos'
+
+
+class TodoInputContainer extends Component {
+  id = 1
+  getId = () => {
+    return ++this.id
+  }
+
+  handleChange = (e) => {
+    const { value } = e.target
+    const { InputActions } = this.props
+    InputActions.setInput(value)
+  }
+
+  handleInsert = () => {
+    const { InputActions, TodosActions, value } = this.props
+    const todo = {
+      id: this.getId(),
+      text: value,
+      done: false
+    }
+    TodosActions.insert(todo)
+    InputActions.setInput('')
+  }
+
+  render() {
+    const { value } = this.props
+    const { handleChange, handleInsert } = this
+
+    return (
+      <div>
+        <TodoInput
+          onChange={handleChange}
+          onInsert={handleInsert}
+        />
+      </div>
+    )
+  }
+}
+
+//MapStateToProps, mapDispatchToProps 함수를 만들지 않고, 내부에 바로 작성
+export default connect(
+  (state) => ({
+    value: state.input.get('value')
+  }),
+  (dispatch) => ({
+    InputActions: bindActionCreators(inputActions, dispatch),
+    TodosActions: bindActionCreators(todosActions, dispatch)
+  })
+)(TodoInputContainer)
