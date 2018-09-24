c42018d612217c443f4e57f40889b3f23d7d46f1
seungdols <seungdols0822@gmail.com>
2018-09-25 00:17:28 +0900

    Add Root component

--- react-router-tutorial/src/App.js
+++ react-router-tutorial/src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      리액트 라우터를 배워봅시다.
+    </div>
+  )
 }
 
 export default App;
