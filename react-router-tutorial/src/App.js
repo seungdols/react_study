15581baa60a0565f9fdace6d67fb8f9e79a89029
seungdols <seungdols0822@gmail.com>
2018-09-25 00:53:23 +0900

    Apply Link Component

--- react-router-tutorial/src/App.js
+++ react-router-tutorial/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import { Home, About } from './pages'
+import Menu from './components/Menu'
 
 const App = () => {
   return (
     <div>
+      <Menu />
       <Route exact path="/" component={Home} />
       <Route path="/about/:name?" component={About} />
     </div>
