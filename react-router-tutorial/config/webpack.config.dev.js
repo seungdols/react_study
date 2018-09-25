2185c470ee9ba012481c60082253aeaeefeaed21
seungdols <seungdols0822@gmail.com>
2018-09-25 23:25:23 +0900

    17. code spliting: Modify plugin of webpack

--- react-router-tutorial/config/webpack.config.dev.js
+++ react-router-tutorial/config/webpack.config.dev.js
@@ -204,6 +204,10 @@ module.exports = {
     ],
   },
   plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.js'
+    }),
     // Makes some environment variables available in index.html.
     // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
