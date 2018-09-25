adfddcabde9c39afa86db307a27cd3a5228fb398
seungdols <seungdols0822@gmail.com>
2018-09-25 23:38:36 +0900

    17. code spliting: Delete practice file, Add asyncComponent func

--- react-router-tutorial/src/App.js
+++ react-router-tutorial/src/App.js
@@ -2,13 +2,11 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { Home, About, Posts } from './pages'
 import Menu from './components/Menu'
-import AsyncSplitMe from './components/AsyncSplitMe'
 
 const App = () => {
   return (
     <div>
       <Menu />
-      <AsyncSplitMe />
       <Route exact path="/" component={Home} />
       <Switch>
         <Route path="/about/:name" component={About} />
