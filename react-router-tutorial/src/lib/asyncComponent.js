bba057ff110f64e42a9dd8f632798ebbcb817679
seungdols <seungdols0822@gmail.com>
2018-09-28 01:19:31 +0900

    17.2 🐛 Fix error

--- react-router-tutorial/src/lib/asyncComponent.js
+++ react-router-tutorial/src/lib/asyncComponent.js
@@ -2,26 +2,27 @@ import React from 'react'
 
 export default function asyncComponent(getComponent) {
   return class AsyncComponent extends React.Component {
-    static Component = null
+    static Component = null;
     state = {
       Component: AsyncComponent.Component
-    }
+    };
 
     constructor(props) {
-      super(props)
+      super(props);
       if (AsyncComponent.Component) return;
       getComponent().then(({default: Component}) => {
         AsyncComponent.Component = Comment
         this.setState({Comment})
-      })
+      });
     }
-  }
+
 
   render() {
-    const { Component } = this.state
-    if(Component) {
-      return <Component {...this.props}/>
+      const { Component } = this.state
+      if(Component) {
+        return <Component {...this.props}/>
+      }
+      return null
     }
-    return null
   }
 }
