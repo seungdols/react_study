991bbad4288c53071de7534578f1969878cf0ae8
seungdols <seungdols0822@gmail.com>
2018-10-02 00:17:35 +0900

    18.5 api 모듈화

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,26 +1,10 @@
 const Koa = require('koa');
 const Router = require('koa-router')
-
+const api = require('./api')
 const app = new Koa();
 const router = new Router();
 
-router.get('/', (ctx) => {
-  ctx.body = 'Home';
-});
-
-router.get('/about', (ctx) => {
-  ctx.body = 'Introduce';
-});
-
-router.get('/about/:name?', (ctx) => {
-  const { name } = ctx.params;
-  ctx.body = name ? `introduce of ${name}`:'introduce';
-})
-
-router.get('/posts', (ctx) => {
-  const {id } = ctx.query;
-  ctx.body = id ? `Post#${id}`:'Not PostId'
-});
+router.use('/api', api.routes())
 
 app.use(router.routes()).use(router.allowedMethods())
 
