a687c5e6b37260faee6e0038f5bdc7c2ced5489e
seungdols <seungdols0822@gmail.com>
2018-10-02 00:23:30 +0900

    18.5 RESTAPI 테스트 해보기

--- blog/blog-backend/src/api/index.js
+++ blog/blog-backend/src/api/index.js
@@ -1,9 +1,8 @@
 const Router = require('koa-router')
+const posts = require('./posts')
 
 const api = new Router()
 
-api.get('/test', (ctx) => {
-  ctx.body = 'test success'
-})
+api.use('/posts', posts.routes())
 
 module.exports = api
