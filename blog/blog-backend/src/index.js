cc3d8bf2298379b212d1cf3b334caacd28f62c22
seungdols <seungdols0822@gmail.com>
2018-10-02 00:14:32 +0900

    18.5 queryParams, query

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -12,6 +12,16 @@ router.get('/about', (ctx) => {
   ctx.body = 'Introduce';
 });
 
+router.get('/about/:name?', (ctx) => {
+  const { name } = ctx.params;
+  ctx.body = name ? `introduce of ${name}`:'introduce';
+})
+
+router.get('/posts', (ctx) => {
+  const {id } = ctx.query;
+  ctx.body = id ? `Post#${id}`:'Not PostId'
+});
+
 app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(4000, () => {
