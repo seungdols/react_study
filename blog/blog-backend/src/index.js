1c1df201e32c9b5cc8f4a02467ba6915613ec29e
seungdols <seungdols0822@gmail.com>
2018-10-01 23:54:36 +0900

    18.5 koa-router 사용해보기

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,27 +1,18 @@
 const Koa = require('koa');
+const Router = require('koa-router')
 
 const app = new Koa();
+const router = new Router();
 
-app.use((ctx, next) => {
-  console.log(1);
-  next().then(() => {
-    console.log('bye')
-  })
-})
-app.use((ctx, next) => {
-  console.log(2);
-  next()
-})
-
-app.use(async (ctx, next) => {
-  console.log(3);
-  await next();
-  console.log('async bye')
-})
-
-app.use((ctx) => {
-  ctx.body = 'hello world';
+router.get('/', (ctx) => {
+  ctx.body = 'Home';
 });
+
+router.get('/about', (ctx) => {
+  ctx.body = 'Introduce';
+});
+
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(4000, () => {
   console.log('listening to port 4000');
