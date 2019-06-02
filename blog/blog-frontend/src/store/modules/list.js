12bc030d5cac1f82f42611aabfd5650f1f6af96e
Seungdols <seungdols0822@gmail.com>
2019-06-02 22:35:21 +0900

    Add store, modules of store

---
+++ blog/blog-frontend/src/store/modules/list.js
@@ -0,0 +1,15 @@
+import {createAction, handleActions} from 'redux-actions';
+import {Map} from 'immutable';
+import {pender} from 'redux-pender';
+
+// action types
+
+// action creators 
+
+// initial state
+const initialState = Map({});
+
+export default handleActions({
+
+}, initialState)
+
